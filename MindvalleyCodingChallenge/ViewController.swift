243c2664783ac12137a784917cf7390ac9a3ac87
Kan Chanproseth (HLB) <KanChanproseth@hlbb.hongleong.com.my>
2020-06-22 15:12:52 +0800

    Initial Commit

---
+++ MindvalleyCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindvalleyCodingChallenge
+//
+//  Created by Kan Chanproseth (HLB) on 22/06/2020.
+//  Copyright © 2020 Kan Chanproseth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
